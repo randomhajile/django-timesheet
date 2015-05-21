c23b15618b6e05523cb017a6052215313527ab51
Mike Hampton <mhampton@rentjungle.com>
2015-05-20 21:06:57 -0400

    installation script added

---
+++ README.md
@@ -0,0 +1,33 @@
+=========
+Timesheet
+=========
+
+Timesheet is a simple Django app for invoicing.
+
+Quick Start
+-----------
+
+1. Add "timesheet" to your INSTALLED_APPS setting like this::
+```
+    INSTALLED_APPS = (
+        ...
+        'timesheet',
+    )
+```
+2. Include the polls URLconf in your project urls.py like this::
+
+    url(r'^timesheet/', include('timesheet.urls', namespace='timesheet')),
+
+3. Run `python manage.py migrate` to create the polls models.
+
+4. Visit http://127.0.0.1:8000/timesheet/ to participate in the poll.
+
+Installation Instructions
+-------------------------
+1. Clone this repository.
+
+2. Run `python setup.py sdist`.
+
+3. Install the package using `pip install django-timesheet/dist/django-timesheet-0.1.tar.gz`.
+
+4. Follow the Quick Start section.
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-timesheet',
+    version='0.1',
+    packages=['timesheet'],
+    include_package_data=True,
+    license='BSD License',
+    description='A simple Django app for invoicing.',
+    long_description=README,
+    author='Mike Hampton',
+    author_email='mhampto1@binghamton.edu',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
